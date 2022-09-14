98f4d645e66def1f0033b95e7f6620b65e4c2afd
NadjaR12 <nadja.rathenow@rub.de>
2022-09-14 15:52:17 +0200

    Linked LandingPage to Home

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import PinkExplosionIMG from "../assets/images/explosion-medium.png";
 
 export default function LandingPage() {
-  return <div>LandingPage</div>;
+  return (
+    <div>
+      <Link to={"/home"}>
+        <img src={PinkExplosionIMG} alt="explosion" />
+      </Link>
+    </div>
+  );
 }
