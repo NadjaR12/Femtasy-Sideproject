bd433eae265021087a455d44fe346f970ad981d8
NadjaR12 <nadja.rathenow@rub.de>
2022-09-15 12:25:58 +0200

    Deleted LandingPage for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
 import Home from "./pages/Home";
 
 export default function App() {
@@ -9,7 +8,6 @@ export default function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LandingPage />} />
           <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
