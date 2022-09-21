72eb4d7c107aadc8419396b4ff8f897399d21ce7
NadjaR12 <nadja.rathenow@rub.de>
2022-09-21 12:52:05 +0200

    Added styles for LandingPage

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PinkExplosionIMG from "../../assets/images/explosion-medium.png";
+import FemtasyLogo from "../../assets/images/femtasy-logo.png";
 
-import "./style.scss";
+import "./styles.scss";
 
 export default function LandingPage() {
   return (
-    <div>
+    <div className="LandingPage__bg">
       <Link to={"/home"}>
-        <img src={PinkExplosionIMG} alt="explosion" />
+        <div className="LandingPage__logo-container">
+          <img
+            className="LandingPage__logo-bg"
+            src={PinkExplosionIMG}
+            alt="explosion"
+          />
+          <img
+            className="LandingPage__logo"
+            src={FemtasyLogo}
+            alt="explosion"
+          />
+        </div>
       </Link>
     </div>
   );
