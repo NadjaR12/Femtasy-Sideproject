53b8042c6b252576a9d46adb579bfb6dfc4fc811
NadjaR12 <nadja.rathenow@rub.de>
2022-09-21 12:53:40 +0200

    Added test for StoryList

---
+++ src/components/StoryList/index.test.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+// import { render } from "@testing-library/react";
+// import "@testing-library/jest-dom";
+
+// import StoryList from ".";
+
+// const stories = [
+//   {
+//     id: "1",
+//     title: "Story 1",
+//     description: "This story is about this and that.",
+//   },
+//   {
+//     id: "2",
+//     title: "Story 2",
+//     description: "This story is about this and that.",
+//   },
+// ];
+
+// interface IProps {
+//   stories: any[];
+// }
+// const mountComponent = ({ stories }: IProps) => {
+//   return render(<StoryList stories={stories} />);
+// };
+
+// describe("StoryList", () => {});
