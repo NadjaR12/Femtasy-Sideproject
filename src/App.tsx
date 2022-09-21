113fd554b77b3e69a54cb36ea5ad0d5cb32641c7
NadjaR12 <nadja.rathenow@rub.de>
2022-09-21 09:49:15 +0200

    Created LandingPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Home from "./pages/Home";
+import LandingPage from "./pages/LandingPage";
 
 export default function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<LandingPage />} />
           <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
