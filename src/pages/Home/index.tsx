f74b0ab008b8e81917708464595d6db28ab0198a
NadjaR12 <nadja.rathenow@rub.de>
2022-09-23 18:26:51 +0200

    Added bg styles

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,9 +3,11 @@ import React from "react";
 import PageSkeleton from "../../components/PageSkeleton";
 import StoryList from "../../components/StoryList";
 
+import "./styles.scss";
+
 export default function Home() {
   return (
-    <div>
+    <div className="Home__bg">
       <PageSkeleton title="Home" subtitle="Overview what to find on that side">
         <StoryList />
       </PageSkeleton>
