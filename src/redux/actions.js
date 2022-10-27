700e043e96eb8364138dc421927926cb0d98624d
NadjaR12 <nadja.rathenow@rub.de>
2022-10-27 17:37:42 +0200

    Added Action

---
+++ src/redux/actions.js
@@ -0,0 +1,12 @@
+// ACTON CREATORS
+export const showLoaderSpinner = () => {
+  return {
+    type: "SHOW_LOADERSPINNER",
+  };
+};
+
+export const hideLoaderSpinner = () => {
+  return {
+    type: "HIDE_LOADERSPINNER",
+  };
+};
