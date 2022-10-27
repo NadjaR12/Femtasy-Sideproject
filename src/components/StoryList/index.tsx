9908d7aeffd6f8c159678dd2636a15c27447710c
NadjaR12 <nadja.rathenow@rub.de>
2022-10-27 17:38:03 +0200

    Added redux logiv for LoaderSpinner to StoryList

--- src/components/StoryList/index.tsx
+++ src/components/StoryList/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 
+import { useSelector, useDispatch } from "react-redux";
+import { showLoaderSpinner } from "../../redux/actions";
+import { hideLoaderSpinner } from "../../redux/actions";
+
 import Header from "../Header";
+import LoaderSpinner from "../LoaderSpinner";
 import StoryCard from "../StoryCard";
 
 import useGetAllStories from "../../hooks/useGetAllStories";
@@ -8,20 +13,31 @@ import useGetAllStories from "../../hooks/useGetAllStories";
 import "./styles.scss";
 
 export default function StoryList() {
+  const dispatch = useDispatch();
+  const loading = useSelector((state: any) => state.loading);
+
   const stories = useGetAllStories();
+
+  if (stories) {
+    dispatch(hideLoaderSpinner());
+  }
 
   return (
     <div>
       <Header title="Femtasy Audios" subtitle="List of All Audios" />
-      <div className="StoryList">
-        {stories.map((story) => {
-          return (
-            <div key={story.id}>
-              <StoryCard story={story} />
-            </div>
-          );
-        })}
-      </div>
+      {loading ? (
+        <LoaderSpinner />
+      ) : (
+        <div className="StoryList">
+          {stories.map((story) => {
+            return (
+              <div key={story.id}>
+                <StoryCard story={story} />
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
