c508626e6f2724a6f5ad6ad7e66033031b3e0146
NadjaR12 <nadja.rathenow@rub.de>
2022-11-01 09:52:59 +0100

    Added LoaderSpinner component

--- src/components/LoaderSpinner/index.tsx
+++ src/components/LoaderSpinner/index.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { Circles } from "react-loader-spinner";
+
+import "./styles.scss";
 
 export default function LoaderSpinner() {
-  return <div className="LoaderSpinner">....Loading</div>;
+  return (
+    <div className="LoaderSpinner">
+      <Circles
+        height="100"
+        width="100"
+        color="#4fa94d"
+        ariaLabel="circles-loading"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
+    </div>
+  );
 }
