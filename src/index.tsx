aebd85a07c7cfabc60931e073ceb17c516d9661e
NadjaR12 <nadja.rathenow@rub.de>
2022-11-08 16:34:22 +0100

    Renamed store to reduxStore

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import store from "./redux/store";
+import reduxStore from "./redux/store";
 import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider store={reduxStore}>
       <App />
     </Provider>
   </React.StrictMode>
